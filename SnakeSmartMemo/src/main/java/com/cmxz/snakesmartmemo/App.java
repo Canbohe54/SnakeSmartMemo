8ae8303b6f82a2d12255392fce4feadab462ebb6
Canbohe54 <Canbohe54@outlook.com>
2022-10-30 00:49:07 +0800

    test

---
+++ SnakeSmartMemo/src/main/java/com/cmxz/snakesmartmemo/App.java
@@ -0,0 +1,13 @@
+package com.cmxz.snakesmartmemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
