8f6f4fca81190a349e0a1f68ebae0700a328974b
mky <1748438065@qq.com>
2022-10-30 13:09:07 +0800

    mky test

--- SnakeSmartMemo/src/main/java/com/cmxz/snakesmartmemo/App.java
+++ SnakeSmartMemo/src/main/java/com/cmxz/snakesmartmemo/App.java
@@ -4,11 +4,12 @@ package com.cmxz.snakesmartmemo;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         //test
+        System.out.println("mky");
     }
 }
