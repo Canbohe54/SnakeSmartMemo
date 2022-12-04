702a3595a8e58e795fd275d0c90bfc96f7c1c1f2
Canbohe54 <Canbohe54@outlook.com>
2022-12-04 20:09:11 +0800

    Add Login Web Page

--- SnakeSmartMemo/src/main/web/vue_notepad/src/App.vue
+++ SnakeSmartMemo/src/main/web/vue_notepad/src/App.vue
@@ -3,6 +3,7 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <router-link to="/Login">Login</router-link>
     </nav>
     <router-view/>
   </div>
