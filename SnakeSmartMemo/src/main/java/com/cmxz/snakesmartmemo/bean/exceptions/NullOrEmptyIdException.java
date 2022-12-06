17385ee5d886bbe95cbcf730be41b621ff6a2a4b
mky <1748438065@qq.com>
2022-12-06 23:02:38 +0800

    update login

---
+++ SnakeSmartMemo/src/main/java/com/cmxz/snakesmartmemo/bean/exceptions/NullOrEmptyIdException.java
@@ -0,0 +1,7 @@
+package com.cmxz.snakesmartmemo.bean.exceptions;
+
+public class NullOrEmptyIdException extends RuntimeException{
+    public NullOrEmptyIdException(){
+        super("NullOrEmptyIdException");
+    }
+}
