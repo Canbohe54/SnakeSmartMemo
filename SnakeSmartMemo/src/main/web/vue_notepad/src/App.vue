b27d649cf5c7de03ce90496e1519d56412b30df5
Canbohe54 <Canbohe54@outlook.com>
2022-12-11 19:16:24 +0800

    add notepad page, update header design

--- SnakeSmartMemo/src/main/web/vue_notepad/src/App.vue
+++ SnakeSmartMemo/src/main/web/vue_notepad/src/App.vue
@@ -1,9 +1,10 @@
 <template>
-  <div id="app">
-    <el-header>
-      <el-row>
+  <el-container id="app" direction="vertical">
+    <el-header class="navHeader">
+      <!-- <el-row>
         <el-col> </el-col>
         <el-col>
+          <div class="logo"></div>
           <el-menu
             default-active="notepad"
             class="el-menu-demo"
@@ -18,15 +19,39 @@
             >
           </el-menu>
         </el-col>
+      </el-row> -->
+      <el-row>
+        <span class="logo"></span>
+        <span class="title">蛇蛇智能备忘录</span>
+        <el-tabs v-model="activeName" @tab-click="handleClick" class="navTabs">
+        <el-tab-pane label="备忘录" name="notepad"></el-tab-pane>
+        <el-tab-pane label="登录/注册" name="login"></el-tab-pane>
+      </el-tabs>
       </el-row>
     </el-header>
     <el-main>
-          <router-view />
+      <router-view />
     </el-main>
-
-  </div>
+  </el-container>
 </template>
 <script>
+  export default {
+    data() {
+      return {
+        activeName: (this.$router.currentRoute.name=='login'?'login':'notepad')
+      };
+    },
+    methods: {
+      handleClick(tab, event) {
+        if(tab.name=="login"){
+          this.$router.push("/login");
+        }
+        if(tab.name=="notepad"){
+          this.$router.push("/");
+        }
+      }
+    }
+  };
 </script>
 <style>
 #app {
@@ -35,6 +60,8 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  width: 100%;
+  height: 100%;
 }
 
 nav {
@@ -49,6 +76,7 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
+
 .notepad {
   margin: 0 0 0 120px !important;
 }
@@ -56,4 +84,42 @@ nav a.router-link-exact-active {
   float: right !important;
   margin: 0 20px 0 0 !important;
 }
+.navHeader {
+  background-color: whitesmoke;
+  border-radius: 5px;
+}
+
+.logo {
+  display: flex;
+  float: left;
+  height: 40px;
+  width: 40px;
+  margin: 10px 10px;
+  background: url("assets/snake_emoji.png") no-repeat center center;
+  background-size: contain;
+}
+.title {
+  display: flex;
+  float: left;
+  margin: 10px 10px;
+  font: normal 400 20px/40px "Microsoft YaHei";
+}
+.navHeader .el-tabs {
+  float: right;
+  margin: 0 30px;
+}
+.navHeader .el-tabs__item {
+  padding: 10px 30px 50px;
+  font: normal 400 18px/40px "Microsoft YaHei";
+}
+.navHeader .el-tabs__nav-wrap::after {
+  background: none;
+}
+/* .el-menu-demo {
+  display: inline-block;
+  height: max-content;
+}
+.el-menu.el-menu--horizontal {
+  border-bottom: 0;
+} */
 </style>
--- SnakeSmartMemo/src/main/web/vue_notepad/src/views/NotepadView.vue
+++ SnakeSmartMemo/src/main/web/vue_notepad/src/views/NotepadView.vue
@@ -1,12 +1,74 @@
 <template>
-  <div>
-    <h1>笔记本正在开发中……</h1>
-  </div>
+  <el-container direction="horizonal">
+    <el-container id="sidePane"></el-container>
+    <el-container direction="vertical">
+    <el-row class="tools">
+      <el-button icon="el-icon-finished" @click="sidePaneVisual">{{openCloseSide}}事件列表</el-button>
+      <el-button icon="el-icon-microphone" @click="log()">语音输入</el-button>
+      <el-button icon="el-icon-download" @click="log()">保存到本地</el-button>
+      <el-button icon="el-icon-document-checked" @click="log()">智能事件识别</el-button>
+      <el-button icon="el-icon-upload">上传到云端</el-button>
+    </el-row>
+    <!-- <el-button @click="formtoHtml">转换为Html样式</el-button> -->
+    <div class="textEditor">
+      <vue-html5-editor
+      content=""
+        v-model="ruleForm.content"
+        height=600
+        show-module-name="true"
+        @change="updateData"
+      ></vue-html5-editor>
+    </div>
+  </el-container>
+  
+  </el-container>
+  
 </template>
 <script>
 export default {
-    name: 'NotepadView',
-    components: {
+  name: "NotepadView",
+  components: {},
+  data() {
+    return {
+      openCloseSide:"关闭",
+      ruleForm: {
+        content: "hello",
+      },
+    };
+  },
+  methods: {
+    updateData: function (data) {
+      this.ruleForm.content = data;
+      data = this.ruleForm.content;
+    },
+    log(){
+      console.log(this.ruleForm.content);
+    },
+    formtoHtml(){
+      document.execCommand("") //改进之处：可以转换为html文档
+    },
+    sidePaneVisual(){//开启/关闭侧边栏
+      let sidePaneClass = document.getElementById('sidePane');
+      if(sidePaneClass.getAttribute("style")){
+        sidePaneClass.removeAttribute("style");
+        this.openCloseSide="关闭";
+      }else{
+        sidePaneClass.setAttribute("style","display: none!important;");
+        this.openCloseSide="打开";
+      }
+      
     }
-}
+  },
+};
 </script>
+<style>
+.tools {
+  margin: 0 0 20px 0;
+}
+#sidePane {
+  background-color:powderblue;
+  height:auto;
+  margin: 0 10px 0 0;
+  border-radius: 5px;
+}
+</style>
