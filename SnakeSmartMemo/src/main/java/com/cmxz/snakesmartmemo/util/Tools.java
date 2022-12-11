54e70efc817790c2a068d99394c03d61210092d4
Mizuiro_ <1170789725@qq.com>
2022-12-11 16:52:27 +0800

    python & java socket update

--- SnakeSmartMemo/src/main/java/com/cmxz/snakesmartmemo/util/Tools.java
+++ SnakeSmartMemo/src/main/java/com/cmxz/snakesmartmemo/util/Tools.java
@@ -13,38 +13,28 @@ public class Tools {
     public static String CallPythonTools(String comm, String data) throws Exception{
         InetAddress addr = InetAddress.getLocalHost();
         String respText;
-        System.out.println(addr.getHostAddress());
         try(DatagramSocket sock = new DatagramSocket(50310, addr)){
             sock.connect(addr, 11451);
-            System.out.println(sock.getLocalSocketAddress());
-            System.out.println(sock.getRemoteSocketAddress());
-            String requestData = "{'api':" + comm + ",'data':" + data + "}";
+            String requestData = "{\"api\":" + comm + ",\"data\":" + data + "}";
+            String requestLen = "{\"len\":" + requestData.getBytes(StandardCharsets.UTF_8).length + "}";
+            System.out.println(requestLen);
             System.out.println(requestData);
-            DatagramPacket packet = new DatagramPacket(requestData.getBytes(StandardCharsets.UTF_8),
+            DatagramPacket packet = new DatagramPacket(requestLen.getBytes(StandardCharsets.UTF_8),
+                    requestLen.getBytes(StandardCharsets.UTF_8).length);
+            sock.send(packet);
+            packet = new DatagramPacket(requestData.getBytes(StandardCharsets.UTF_8),
                     requestData.getBytes(StandardCharsets.UTF_8).length);
             sock.send(packet);
-            System.out.println("The data was send to server.");
-            System.out.print(packet.getAddress().getHostAddress());
-            System.out.println("  " + packet.getPort());
-            byte[] buffer = new byte[1024];
+            byte[] buffer = new byte[1024*1024];
             DatagramPacket resp = new DatagramPacket(buffer, buffer.length);
             sock.receive(resp);
             byte[] respData = resp.getData();
             respText = new String(respData, 0, resp.getLength(), StandardCharsets.UTF_8);
-            System.out.println(respText);
         }
-
         return respText;
     }
 
-    //public static void main(String[] args) throws Exception {
-    //    System.out.println(CallPythonTools("time.parser", "[\"2015年10月3日去上学\", {}]"));
-//        try{
-//            System.out.println(CallPythonTools("time.parser", "[\"2015年10月3日去上学\", {}]"));
-//        }
-//        catch (Exception e){
-//            System.out.println("Error. " + e.getMessage());
-//        }
-
-    //}
+    public static void main(String[] args) throws Exception{
+        System.out.println(CallPythonTools("\"time.parser\"", "[\"2015年10月3日去上学\", {}]"));
+    }
 }
