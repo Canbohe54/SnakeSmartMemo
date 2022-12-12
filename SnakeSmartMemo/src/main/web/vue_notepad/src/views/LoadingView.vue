ff80c63f368386b517c7fc1a085f89a14ad22fd5
Canbohe54 <Canbohe54@outlook.com>
2022-12-12 16:28:43 +0800

    add button limits

---
+++ SnakeSmartMemo/src/main/web/vue_notepad/src/views/LoadingView.vue
@@ -0,0 +1,20 @@
+<template>
+      <el-skeleton :rows="20" animated />
+  </template>
+  <script>
+export default {
+  
+  created(){
+        this.$router.push({
+                path: "/",
+              });
+      }
+    
+}
+</script>
+<style>
+.el-skeleton {
+  float: left;
+}
+</style>
+  
