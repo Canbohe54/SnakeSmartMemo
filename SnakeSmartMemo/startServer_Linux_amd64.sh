f00a5d72ead2e09e4b631510618e573dc3679a28
Canbohe54 <Canbohe54@outlook.com>
2022-12-20 01:51:00 +0800

    Add upload, share to the page

---
+++ SnakeSmartMemo/startServer_Linux_amd64.sh
@@ -0,0 +1,3 @@
+java --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED Starter.jar
+cd src\main\python
+python3 start.py
